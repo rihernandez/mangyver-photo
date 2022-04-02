38ee75c63db9fd7e6fde34dce504a6495e22a0b9
rhc921004@gmail.com <rhc921004@gmail.com>
2022-04-01 22:48:43 -0400

    chore: adding database configuration

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,6 @@
 import { connect } from 'mongoose'
 
 export async function startConnection() {
-    const db = await connect('mongodb://localhost/mean-gallery',{});
+    const db = await connect('mongodb+srv://root:toor@cluster0.pb9qa.mongodb.net/mangyver-photo?retryWrites=true&w=majority',{});
     console.log('Database is connected');
 }
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { startConnection } from './database'
 
 async function main() {
     startConnection();
-    await app.listen(app.get('port'));
+    app.listen(app.get('port'));
     console.log('Server on port', app.get('port'));
 }
 
